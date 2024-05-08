503fd5526362d1ca03c30f7fb3cccf2fc6816f8b
aipacino123 <jyothishankit@gmail.com>
2024-05-08 00:58:02 -0500

    Added changes for the simulator

--- simulator.py
+++ simulator.py
@@ -26,28 +26,43 @@ class FleetSimulator(object):
         requests = get_requests_with_offset(self.requests, self.current_time, num_steps, offset)
         return requests
 
-    def step(self, actions=None):
-        num_steps = get_number_of_steps(self.cycle)
+    def calculate_num_steps(self):
+        return get_number_of_steps(self.cycle)
+
+    def process_actions(self, actions):
         if actions:
             self.dispatch(actions)
+
+    def get_and_process_requests(self, num_steps):
         requests = self.get_requests(num_steps)
+        current_time = self.current_time
+        current_requests = get_requests_without_offset(requests, current_time)
+        return current_requests
+
+    def run_step_iterations(self, num_steps, current_requests):
         wait = 0
         reject = 0
         gas = 0
         request_hop_zero = 0
-        for iter in range(num_steps):
+        for _ in range(num_steps):
             for vehicle in self.vehicles:
                 gas += vehicle.transition()
             vehicle_locations = self.get_vehicles_location()
-            current_requests = get_requests_without_offset(requests, self.current_time)
             assignments = self.match(vehicle_locations, current_requests)
-            current_wait,num_vehicles,num_passengers,request_hop_zero_current = self.assign(assignments)
+            current_wait, num_vehicles, num_passengers, request_hop_zero_current = self.assign(assignments)
             reject += len(current_requests) - num_passengers
             wait += current_wait
             request_hop_zero += request_hop_zero_current
             self.update_time()
+        return wait, reject, gas, request_hop_zero
+
+    def step(self, actions=None):
+        num_steps = self.calculate_num_steps()
+        self.process_actions(actions)
+        current_requests = self.get_and_process_requests(num_steps)
+        wait, reject, gas, request_hop_zero = self.run_step_iterations(num_steps, current_requests)
         vehicles = self.get_vehicles_dataframe()
-        return vehicles, requests, wait, reject, gas, request_hop_zero
+        return vehicles, current_requests, wait, reject, gas, request_hop_zero
     
     def get_vehicles_dataframe(self):
         vehicles = [vehicle.get_state() for vehicle in self.vehicles]
@@ -61,109 +76,48 @@ class FleetSimulator(object):
     def get_vehicles_score(self):
         vehicles = get_vehicle_reward_service_time_idle_time(vehicles)
         return vehicles
-    
+
     def match(self, resources, tasks):
-        R = resources[resources.available == 1]
-        tasks_uniq = tasks.groupby(['plat','plon']).count()
-        tasks_uniq = tasks_uniq.reset_index(level=['plat','plon'])
-        tasks_uniq = tasks_uniq[['plat','plon']]  
-        tasks_uniq['index']=tasks_uniq.apply(lambda rows:tasks[(tasks['plat']==rows['plat'].item()) & 
-                            (tasks['plon']==rows['plon'].item())].index.values  , axis=1)
-        
-        d = gh.distance_in_meters(R.lat.values,
-                                  R.lon.values,
-                                  tasks_uniq.plat.values[:, None],
-                                  tasks_uniq.plon.values[:, None])
-        N = min(len(tasks_uniq), len(R))
-        vids = np.zeros(N, dtype=int)
-        for i in range(N):
-            vid = d[i].argmin()
-            if d[i, vid] > REJECT_DISTANCE:
-                vids[i] = -1
-            else:
-                vids[i] = vid
-                d[:, vid] = float('inf')
-        index_values = tasks_uniq.index[:N][vids >= 0]
-        assignments = list(zip(tasks_uniq.loc[index_values]['index'], R['id'].iloc[vids[vids >= 0]]))
+        available_resources = find_available_resources(resources)
+        unique_tasks = count_unique_tasks(tasks)
+        task_indices = find_task_indices(tasks, unique_tasks)
+        assignments = assign_resources_to_tasks(available_resources, unique_tasks, task_indices)
         return assignments
 
-
     def assign(self, assignments):
-        num_vehicles = len(assignments)
-        request_hop_zero =0 
-        num_passengers = 0
-        wait = 0
-        for r, v in assignments:
-            vehicle = self.vehicles[v]
-            request = self.requests.loc[r]
-            num_present_vehicle_pass = len(request) 
-            request_hop_zero += sum(request['hop_flag']==0)
-            num_passengers += num_present_vehicle_pass
-            request_sort = request.sort_values(by=['trip_time'])
-            first_row = request_sort.iloc[0]
-            last_row = request_sort.iloc[-1]
-            ploc = (first_row.plat, first_row.plon)
-            dloc = (last_row.dlat, last_row.dlon)
-            vloc = vehicle.location
-            d = gh.distance_in_meters(vloc[0], vloc[1], ploc[0], ploc[1])
-            wait_time = (d * 2 / 1.414) / (ASSIGNMENT_SPEED * 1000 / 60)
-            eff_distance = sum(request.trip_distance)
-            request_sort_copy = request_sort.copy()
-            request_sort.loc[:,'index_val'] = range(len(request_sort))
-            request_sort_copy.loc[:,'index_val'] = request_sort['index_val'].values -1
-            request_sort_copy = request_sort_copy[['dlat','dlon','index_val']]
-            request_sort_join = pd.merge(request_sort, request_sort_copy, how='left', on=None, left_on='index_val', right_on='index_val',
-                                         left_index=False, right_index=False, sort=False,suffixes=('_x', '_y'), 
-                                         copy=True, indicator=False,validate=None)
-            
-            request_join_no_na = request_sort_join.dropna()
-            if len(request_join_no_na) > 0:
-                request_join_no_na['dist_bw_dest'] = gh.distance_in_meters(request_join_no_na.dlat_x,request_join_no_na.dlon_x,
-                                                                       request_join_no_na.dlat_y,request_join_no_na.dlon_y)
-                
-                actual_distance = first_row.trip_distance + (sum(request_join_no_na.dist_bw_dest))/1000
-                trip_time = (actual_distance/ASSIGNMENT_SPEED)*60
-            else:
-                actual_distance = first_row.trip_distance
-                trip_time = first_row.trip_time
-            vehicle.start_service(dloc, wait_time, trip_time,actual_distance,eff_distance,num_present_vehicle_pass)
-            wait += wait_time
+        num_vehicles_assigned = len(assignments)
+        hop_zero_requests = 0 
+        total_passengers = 0
+        total_wait_time = 0
+        for request_idx, vehicle_idx in assignments:
+            vehicle = self.vehicles[vehicle_idx]
+            request = self.requests.loc[request_idx]
+            num_passengers_in_vehicle = len(request) 
+            hop_zero_requests += count_hopper_flag_zero(request)
+            total_passengers += num_passengers_in_vehicle
+            request_sorted = sort_request_by_trip_time(request)
+            first_request = request_sorted.iloc[0]
+            last_request = request_sorted.iloc[-1]
+            pickup_location = (first_request.plat, first_request.plon)
+            dropoff_location = (last_request.dlat, last_request.dlon)
+            vehicle_location = vehicle.location
+            distance_to_pickup = calculate_distance(vehicle_location, pickup_location)
+            wait_time = calculate_wait_time(distance_to_pickup)
+            effective_distance = sum(request.trip_distance)
+            actual_distance, trip_time = calculate_actual_trip_time(request_sorted)
+            vehicle.start_service(dropoff_location, wait_time, trip_time, actual_distance, effective_distance, num_passengers_in_vehicle)
+            total_wait_time += wait_time
 
-        return wait,num_vehicles,num_passengers,request_hop_zero
+        return total_wait_time, num_vehicles_assigned, total_passengers, hop_zero_requests
 
     def dispatch(self, actions):
-        cache = []
-        distances = []
-        vids, targets = zip(*actions)
-        vlocs = [self.vehicles[vid].location for vid in vids]
-        for vloc, tloc in zip(vlocs, targets):
-            try:
-                p, d, s, t = self.router.map_matching_shortest_path(vloc, tloc)
-                cache.append((p, s, t))
-                distances.append(d)
-            except:
-                start_lat = vloc[0]
-                start_lon = vloc[1]
-                dest_lat = tloc[0]
-                dest_lon = tloc[1]
-                d = gh.distance_in_meters(start_lat,start_lon, dest_lat,dest_lon)
-                distances.append(d)
+        vehicle_locations, target_locations = prepare_vehicle_and_target_locations(self, actions)
+        distances = calculate_distances(self, vehicle_locations, target_locations)
+        feature_matrix = prepare_feature_matrix(self, vehicle_locations, target_locations, distances)
+        trip_times = self.eta_model.predict(feature_matrix)
+        execute_actions(self, actions, trip_times)
 
-        N = len(vids)
-        X = np.zeros((N, 7))
-        X[:, 0] = self.dayofweek
-        X[:, 1] = self.minofday / 60.0
-        X[:, 2:4] = vlocs
-        X[:, 4:6] = targets
-        X[:, 6] = distances
-        trip_times = self.eta_model.predict(X)
 
-        for i, vid in enumerate(vids):
-            if trip_times[i] > MIN_TRIPTIME:
-                eta = min(trip_times[i], self.max_action_time)
-                self.vehicles[vid].route([], eta)
-        return
-    
     def get_vehicles_location(self):
         vehicles = [vehicle.get_location() for vehicle in self.vehicles]
         vehicles = pd.DataFrame(vehicles, columns=['id', 'lat', 'lon', 'available'])
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import _pickle as pickle
 import numpy as np
 from vehicle import *
 import pandas as pd
+import geohelper as gh
 
 def load_graph():
     with open(GRAPH_PATH, 'rb') as f:
@@ -50,3 +51,103 @@ def get_vehicle_reward_service_time_idle_time(vehicles):
     vehicles_dataframe_input =[vehicle.get_score() for vehicle in vehicles]
     vehicles_dataframe = pd.DataFrame(vehicles_dataframe_input, columns=['id', 'reward', 'service_time', 'idle_time'])
     return vehicles_dataframe
+
+def find_available_resources(resources):
+        available_resources = resources[resources.available == 1]
+        return available_resources
+
+def count_unique_tasks(tasks):
+    unique_tasks = tasks.groupby(['plat','plon']).count()
+    unique_tasks = unique_tasks.reset_index(level=['plat','plon'])
+    unique_tasks = unique_tasks[['plat','plon']]
+    return unique_tasks
+
+def find_task_indices(tasks, unique_tasks):
+    task_indices = unique_tasks.apply(lambda row: tasks[(tasks['plat']==row['plat']) & 
+                                                        (tasks['plon']==row['plon'])].index.values, 
+                                    axis=1)
+    return task_indices
+
+def assign_resources_to_tasks(available_resources, unique_tasks, task_indices):
+    distances = gh.distance_in_meters(available_resources.lat.values,
+                                    available_resources.lon.values,
+                                    unique_tasks.plat.values[:, None],
+                                    unique_tasks.plon.values[:, None])
+    num_tasks = min(len(unique_tasks), len(available_resources))
+    assigned_resources = np.zeros(num_tasks, dtype=int)
+    for i in range(num_tasks):
+        closest_resource_index = distances[i].argmin()
+        if distances[i, closest_resource_index] > REJECT_DISTANCE:
+            assigned_resources[i] = -1
+        else:
+            assigned_resources[i] = closest_resource_index
+            distances[:, closest_resource_index] = float('inf')
+    task_indices_to_use = unique_tasks.index[:num_tasks][assigned_resources >= 0]
+    assignments = list(zip(unique_tasks.loc[task_indices_to_use]['index'], 
+                        available_resources['id'].iloc[assigned_resources[assigned_resources >= 0]]))
+    return assignments
+
+
+def count_hopper_flag_zero(request):
+        return sum(request['hop_flag'] == 0)
+
+def sort_request_by_trip_time(request):
+    return request.sort_values(by=['trip_time'])
+
+def calculate_distance(loc1, loc2):
+    return gh.distance_in_meters(loc1[0], loc1[1], loc2[0], loc2[1])
+
+def calculate_wait_time(distance):
+    return (distance * 2 / 1.414) / (ASSIGNMENT_SPEED * 1000 / 60)
+
+def calculate_actual_trip_time(request_sorted):
+    request_copy = request_sorted.copy()
+    request_sorted.loc[:, 'index_val'] = range(len(request_sorted))
+    request_copy.loc[:, 'index_val'] = request_sorted['index_val'].values - 1
+    request_copy = request_copy[['dlat','dlon','index_val']]
+    request_join = pd.merge(request_sorted, request_copy, how='left', on='index_val')
+    request_join_no_na = request_join.dropna()
+    if len(request_join_no_na) > 0:
+        request_join_no_na['dist_bw_dest'] = gh.distance_in_meters(request_join_no_na.dlat_x,request_join_no_na.dlon_x,
+                                                                request_join_no_na.dlat_y,request_join_no_na.dlon_y)
+        actual_distance = request_sorted.iloc[0].trip_distance + (sum(request_join_no_na.dist_bw_dest)) / 1000
+        trip_time = (actual_distance / ASSIGNMENT_SPEED) * 60
+    else:
+        actual_distance = request_sorted.iloc[0].trip_distance
+        trip_time = request_sorted.iloc[0].trip_time
+    return actual_distance, trip_time
+
+
+def prepare_vehicle_and_target_locations(self, actions):
+        vehicle_locations = [self.vehicles[vid].location for vid, _ in actions]
+        target_locations = [target for _, target in actions]
+        return vehicle_locations, target_locations
+
+def calculate_distances(self, vehicle_locations, target_locations):
+    distances = []
+    for vloc, tloc in zip(vehicle_locations, target_locations):
+        try:
+            path, distance, _, _ = self.router.map_matching_shortest_path(vloc, tloc)
+            distances.append(distance)
+        except:
+            start_lat, start_lon = vloc
+            dest_lat, dest_lon = tloc
+            d = gh.distance_in_meters(start_lat, start_lon, dest_lat, dest_lon)
+            distances.append(d)
+    return distances
+
+def prepare_feature_matrix(self, vehicle_locations, target_locations, distances):
+    N = len(vehicle_locations)
+    X = np.zeros((N, 7))
+    X[:, 0] = self.dayofweek
+    X[:, 1] = self.minofday / 60.0
+    X[:, 2:4] = vehicle_locations
+    X[:, 4:6] = target_locations
+    X[:, 6] = distances
+    return X
+
+def execute_actions(self, actions, trip_times):
+    for i, (vid, _) in enumerate(actions):
+        if trip_times[i] > MIN_TRIPTIME:
+            eta = min(trip_times[i], self.max_action_time)
+            self.vehicles[vid].route([], eta)
